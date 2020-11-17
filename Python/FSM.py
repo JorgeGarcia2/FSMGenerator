b818b694316c782e6881d8fc7ebfcaaa59373aa7
JorgeGarcia2 <garciav.jorge96@gmail.com>
2020-11-17 17:30:18 -0600

    get files in different directories

--- Python/FSM.py
+++ Python/FSM.py
@@ -15,43 +15,41 @@ class FSM:
 
     def getFile(self):
         noError = True
-        
-        #Ask for the verilog source code file name
-        fileName = input("\nWhat is the table's filename?\n(It should be a csv file) ")
-
-        #Append ".sv" suffix if not found
-        if (not re.match(r"\.csv",fileName)): 
-            fileName = fileName + ".csv"
-
-        #Try to open file
+        fileName = input("\nWhat is the top model's filename? ")
+        #If file has no extension, apend ".v"
+        if (fileName.find(".") == -1): 
+            fileName = fileName + ".v"
         try: 
             f = open(fileName,"r")
-        except:
-            print(f"The {fileName} file was not found or you do not have read permissions\n")
-            fileName=""
-
-            #If cannot open file, check in working directory for other sv files that do not contain "_testbench"
-            for fil in os.listdir("."):
-                if ".sv" in fil and not "_testbench" in fil:
-                    fileName=fil
-
-                    #If a file is found, ask if the user wants to use it
-                    print("sv file found, do you want to use: "+fil+"?(Y,N)\n")
-                    t=input()
-                    #If answer is no, search for more files, else use the file
-                    if t!="n" and t!="N":
-                        print(f"File {fileName} will be used!\n")
-                        f = open(fileName,"r")
-                        break
-                    else:
-                        fileName=""
-                        print("File not used")
-            #If user does not use any file, send error
-            if fileName=="":
-                noError = False
-        #If there is no error, read the file contents and close file
-        if(noError):
-            self.designCode = " " + f.read()
-            print(f"File {fileName} read successfully")
+            self.designCode = f.read()
+            print("File read successfully")
             f.close()
+        except:
+            match = re.search(r"^((\w+\/)*).*$", fileName)
+            print(match.group(1))
+            print(f"The {fileName} file was not found or you do not have read permissions")
+            if(match.group(1) != ""): fileName=match.group(1)
+            else: fileName = "./"
+            if(os.path.isdir(fileName)):
+                for file in os.listdir(fileName):
+                    if(re.search(r"^\w((?!_testbench).)*\.+(sv|v)$", file)):
+                        fileName += file
+                        print("\nDesign file found, do you want to use: "+fileName+"?(Y,N)")
+                        t=input()
+                        if t!="n" and t!="N":
+                            print(f"File {fileName} will be used!")
+                            f = open(fileName,"r")
+                            self.designCode = " "+f.read()
+                            print(f"File {fileName} read successfully")
+                            f.close()
+                            break
+                        else:
+                            if(match.group(1) != ""): fileName=match.group(1)
+                            else: fileName = "./"
+                            print("File not used")
+                if fileName=="./":
+                    noError = False
+            else: 
+                print("The specified path was not found")
+                noError = False
         return noError
