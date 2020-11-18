d7c1c4aae11e943b86d48a3a4e89f84b427ae33f
David Xchel <davidxchelmh@gmail.com>
2020-11-18 00:52:22 -0600

    Bugs fixed

--- Python/Xchel.py
+++ Python/Xchel.py
@@ -3,20 +3,37 @@
 import re
 import os
 
+gsuf = ""
+gkey = ""
+
 #Prompts for valid file name and assigns file text to string
-def getFileCont(suff,Key):
+def getFileCont(suf,key):
+    global gsuf
+    global gkey
+    gkey = key
+    gsuf = suf
+    f = open(getFileName())
+    st = f.read()
+    f.close()
+    return st
+
+def getFileName(suff = "",key = ""):
+    if (suff == ""):
+        suf = gsuf
+        key = gkey
     fileCont = "y"
     #Check if the user wants to continue trying new file names
     while(fileCont == "Y" or fileCont == "y"):
         fileCont = ""
-        fileName = input("\nWhat is the top model's filename?\n(This program only searches for "+suff+" estensions) ")
-        #If file has no extension, apend .suff
+        fileName = input("\nWhat is the top model's filename?\n(This program only searches for "+suf+" estensions) ")
+        #If file has no extension, apend .suf
         if (fileName.find(".") == -1): 
-            fileName = fileName + "." + suff
+            fileName = fileName + "." + suf
         try: 
             f = open(fileName,"r")
-            fileCont = f.read()
-            print(f"File {fileName} read successfully")
+            fileCont = "Hola"
+            #fileCont = f.read()
+            #print(f"File {fileName} read successfully")
             f.close()
         except:
             match = re.search(r"^((\w+\/)*).*$", fileName)
@@ -26,15 +43,16 @@ def getFileCont(suff,Key):
             else: fileName = "./"
             if(os.path.isdir(fileName)):
                 for file in os.listdir(fileName):
-                    if(re.search(r"^\w((?!"+Key+r").)*\."+suff+"$", file)):
+                    if(re.search(r"^\w((?!"+key+r").)*\."+suf+"$", file)):
                         fileName += file
                         print("\nDesign file found, do you want to use: "+fileName+"?(Y,N)")
                         t=input()
                         if t!="n" and t!="N":
                             print(f"File {fileName} will be used!")
                             f = open(fileName,"r")
-                            fileCont = f.read()
-                            print(f"File {fileName} read successfully")
+                            fileCont = "Hola"
+                            #fileCont = f.read()
+                            #print(f"File {fileName} read successfully")
                             f.close()
                             break
                         else:
@@ -47,9 +65,11 @@ def getFileCont(suff,Key):
         #If the program does not find the file, ask to try again
         if (fileCont == ""):
             fileCont = input("\nDo you want to try again? (Y/N)\n")
-            if (fileCont == "N" or fileCont == "n"): fileCont = ""
+            if (fileCont == "N" or fileCont == "n"):
+                fileName = ""
+                fileCont = ""
             else: fileCont = "y"
-    return fileCont
+    return fileName
 
 #Prompts for valid file name and assigns file text to string
 def getFSMSLogic(dic,ppal):
