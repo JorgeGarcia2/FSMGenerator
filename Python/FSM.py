1743bd4c9e38ff7a16929b7940eef6771e225442
David Xchel <davidxchelmh@gmail.com>
2020-11-18 12:35:01 -0600

    Output logic implemented

--- Python/FSM.py
+++ Python/FSM.py
@@ -25,14 +25,15 @@ class FSM:
             print("There is no table!")
         else:
             print("Here's the table "+fileName+"!!\n\n")
-            dictio = Jorch.getFSMDic(fileName)
+            dictio, NI, NO = Jorch.getFSMDic(fileName)
+            print(NI)
+            print(NO)
             self.FSMstr = Fers.getFSMHead(dictio,"FSM")
-            dicKey = {"inputs":["Op"],"ppal":"S0","outputs":["X"]}
-            self.FSMstr += self.getFSMLogic(dictio,dicKey)
+            self.FSMstr += self.getFSMLogic(dictio,NI,NO)
             print(self.FSMstr)
 
     #Makes the state logic for the machine
-    def getFSMLogic(self,dicS,dicK):
+    def getFSMLogic(self,dicS,Ni,No):
         FSMSLogic="\n  //Next State Logic Block\n  always@(state)\n"
         FSMOLogic="\n  //Output Logic Block\n  always@(state)\n"
 
@@ -47,35 +48,35 @@ class FSM:
                 for j in range(len(dicS[S][i][0])):
                     if (dicS[S][i][0][j] != "x" and dicS[S][i][0][j] != "X"):
                         if (f): Temp += " && "
-                        Temp += dicK["inputs"][j] + " == " + dicS[S][i][0][j]
+                        Temp += Ni[j][0] + " == " + dicS[S][i][0][j]
                         f=True
                 if (Temp != "        if("):
                     IF = True
-                    if (len(dicK["outputs"]) > 1):
+                    if (len(No) > 1):
                         FSMOLogic += Temp + ") begin\n"
                     else:
                         FSMOLogic += Temp + ")\n"
                     FSMSLogic += Temp + ")\n"
                 FSMSLogic += "          nextstate = " + dicS[S][i][1] + ";\n\n"
-                for j in range(len(dicK["outputs"])):
+                for j in range(len(No)):
                     if (dicS[S][i][2][j] != "x" and dicS[S][i][2][j] != "X"):
-                        FSMOLogic += "          " + dicK["outputs"][j] + " = " + dicS[S][i][2][j] + ";\n"
-                if (len(dicK["outputs"]) > 1):
+                        FSMOLogic += "          " + No[j][0] + " = " + dicS[S][i][2][j] + ";\n"
+                if (len(No) > 1):
                     FSMOLogic += "        end\n"
                 FSMOLogic += "\n"
             if (IF):
-                FSMSLogic += "        else\n          nextstate = " + dicK["ppal"] + ";\n"
+                FSMSLogic += "        else\n          nextstate = " + "S0" + ";\n"
                 FSMOLogic += "        else begin\n"
-                for j in range(len(dicK["outputs"])):
+                for j in range(len(No)):
                     if (dicS[S][0][2][j] != "x" and dicS[S][0][2][j] != "X"):
-                        FSMOLogic += "          " + dicK["outputs"][j] + " = " + dicS[S][0][2][j] + ";\n"
+                        FSMOLogic += "          " + No[j][0] + " = " + dicS[S][0][2][j] + ";\n"
                 FSMOLogic += "        end\n\n"
             FSMSLogic += "\n"
             FSMOLogic += "\n"
-        FSMSLogic += "      default:\n        nextstate = " + dicK["ppal"] + ";\n"
+        FSMSLogic += "      default:\n        nextstate = " + "S0" + ";\n"
         FSMOLogic += "      default: begin\n"
-        for j in range(len(dicK["outputs"])):
-            FSMOLogic += "        " + dicK["outputs"][j] + " = 0;\n"
+        for j in range(len(No)):
+            FSMOLogic += "        " + No[j][0] + " = " + No[j][1] + "'" + No[j][2] + "0;\n"
         FSMOLogic += "      end\n"
         FSMSLogic += "    endcase\n  end\n"
         FSMOLogic += "    endcase\n  end\n"
@@ -98,21 +99,21 @@ class FSM:
                 for j in range(len(dicS[S][i][0])):
                     if (dicS[S][i][0][j] != "x" and dicS[S][i][0][j] != "X"):
                         if (f): Temp += " && "
-                        Temp += dicK["inputs"][j] + " == " + dicS[S][i][0][j]
+                        Temp += Ni[j] + " == " + dicS[S][i][0][j]
                         f=True
                 if (Temp != "        if("):
                     IF = True
-                    if (opt == 1 and len(dicK["outputs"]) > 1):
+                    if (opt == 1 and len(No) > 1):
                         FSMLogic += Temp + ") begin\n"
                     else:
                         FSMLogic += Temp + ")\n"
                 if (opt == 0):
                     FSMLogic += "          nextstate = " + dicS[S][i][1] + ";\n\n"
                 else:
-                    for j in range(len(dicK["outputs"])):
+                    for j in range(len(No)):
                         if (dicS[S][i][2][j] != "x" and dicS[S][i][2][j] != "X"):
-                            FSMLogic += "          " + dicK["outputs"][j] + " = " + dicS[S][i][2][j] + ";\n"
-                    if (opt == 1 and len(dicK["outputs"]) > 1):
+                            FSMLogic += "          " + No[j] + " = " + dicS[S][i][2][j] + ";\n"
+                    if (opt == 1 and len(No) > 1):
                         FSMLogic += "        end\n"
                     FSMLogic += "\n"
             if (IF):
@@ -120,17 +121,17 @@ class FSM:
                     FSMLogic += "        else\n          nextstate = " + dicK["ppal"] + ";\n"
                 else:
                     FSMLogic += "        else begin\n"
-                    for j in range(len(dicK["outputs"])):
+                    for j in range(len(No)):
                         if (dicS[S][0][2][j] != "x" and dicS[S][0][2][j] != "X"):
-                            FSMLogic += "          " + dicK["outputs"][j] + " = " + dicS[S][0][2][j] + ";\n"
+                            FSMLogic += "          " + No[j] + " = " + dicS[S][0][2][j] + ";\n"
                     FSMLogic += "        end\n\n"
             FSMLogic += "\n"
         if (opt == 0):
             FSMLogic += "      default:\n        nextstate = " + dicK["ppal"] + ";\n"
         else:
             FSMLogic += "      default: begin\n"
-            for j in range(len(dicK["outputs"])):
-                FSMLogic += "        " + dicK["outputs"][j] + " = 0;\n"
+            for j in range(len(No)):
+                FSMLogic += "        " + No[j] + " = 0;\n"
             FSMLogic += "      end\n"
         FSMLogic += "    endcase\n  end\n"
         return FSMLogic"""
