e7ebd4ec200457dbf2bf0ce7652797f6220fe5a4
JorgeGarcia2 <garciav.jorge96@gmail.com>
2020-11-19 15:28:13 -0600

    check outputs vector

--- Cpp/Jorch.cpp
+++ Cpp/Jorch.cpp
@@ -13,5 +13,18 @@ int main(){
     cout << endl;
 	
     s = getFSMData(sstate, inputs, outputs);
-    printFSMDict(s);
+    //printFSMDict(s);
+
+    for (int i=0; i< inputs.size(); i++){
+        cout << "Input: "<< i <<endl;
+        cout << inputs[i][0] << endl;
+        cout << inputs[i][1] << endl;
+        cout << inputs[i][2] << endl;
+    }
+    for (int i=0; i< outputs.size(); i++){
+        cout << "Output: "<< i <<endl;
+        cout << outputs[i][0] << endl;
+        cout << outputs[i][1] << endl;
+        cout << outputs[i][2] << endl;
+    }
 }
