effb9b23dca4802c80837907aa09c32140f232be
mudofer <luisfernando.rodriguez01@gmail.com>
2020-11-19 16:34:03 -0600

    Fixed the statetype variable with brackets and commas {S0, S1, ...}

--- Cpp/Fers.cpp
+++ Cpp/Fers.cpp
@@ -24,18 +24,10 @@ FSMdictionary={{key,vector{FSMLine(),FSMLine()}}.{key,vector{FSMLine(),FSMLine()
 
 string getFSMHead(string& name, FSMdictionary& States, busInfo& input_list, busInfo& output_list)
 {
-    for (int i=0; i< input_list.size(); i++)
-        cout << "entrada: " << input_list[i][0] << endl;
-    for (int i=0; i< output_list.size(); i++)
-        cout << "salida: " << output_list[i][0] << endl;
-
-    cout << endl;
-
     vector<string> keys_vector;     //Store the dictionary keys inside the keys_vector for later use
     for(map<string, Line_vector>::iterator it = States.begin(); it != States.end(); ++it)
     {
         keys_vector.push_back(it->first);
-        //cout << "Key: " << it->first << endl;
     }
 
     //States[keys_vector[i]][j] gets the j-esim object of type FSMLine, whose key is keys_vector[i]
@@ -70,30 +62,33 @@ string getFSMHead(string& name, FSMdictionary& States, busInfo& input_list, busI
         }
     }
 
-    FSMHead += "\n";
     current_bus_size = last_bus_size = 0;
 
     for (unsigned int i = 0; i < output_list.size(); i++)       //For all the outputs of the FSM
     {
         current_bus_size = stoi(output_list[i][1]);         //Store the current bus size
-        if (current_bus_size != last_bus_size)
+        if (current_bus_size != last_bus_size)              //In case that the bus size has changed...
         {
-            FSMHead += "  output reg ";
+            FSMHead += "\n  output reg ";                   //...a new output reg declaration is needed
             if (current_bus_size > 1)
                 FSMHead += "[" + to_string(current_bus_size - 1) + ":0] ";
+            FSMHead += output_list[i][0] + ", ";
         }
-        FSMHead += output_list[i][0] + ",\n";
-        last_bus_size = current_bus_size;
+        else
+            FSMHead += output_list[i][0] + ", ";
+        last_bus_size = current_bus_size;                   //Now store the used bus size as "last bus size"
     }
 
-    FSMHead.erase(FSMHead.end()-2);             //Remove the last two characters ", "
+    FSMHead.erase(FSMHead.end()-2);             //Remove the last two characters
     FSMHead.pop_back();                         //This function seems necessary
 
     string keys_string;                         //Pass the states keys to a string for later use
-    for (const auto &piece : keys_vector) keys_string += piece + " ";
+    for (const auto &piece : keys_vector) keys_string += piece + ", ";
+    keys_string.erase(keys_string.end()-2);     //Remove the last two characters
+    keys_string.pop_back();                     //This function seems necessary
 
     FSMHead += ");\n\n";
-    FSMHead += "  typedef enum reg [" + to_string(required_state_bits-1) + ":0] " + keys_string + "statetype;\n"
+    FSMHead += "  typedef enum reg [" + to_string(required_state_bits-1) + ":0] {" + keys_string + "} statetype;\n"
              + "  statetype state, nextstate;\n\n"
              + "  //State register\n"
              + "  always@ (posedge clock or posedge reset)\n"
@@ -111,21 +106,6 @@ int main()
     FSMdictionary s;
 	
     s = getFSMData(sstate, inputs, outputs);
-
-    /*for (int i=0; i< inputs.size(); i++)
-    {
-        cout << "entrada: " << inputs[i][0] << endl;
-        cout << "tamanio: " << inputs[i][1] << endl;
-        cout << "radix: " << inputs[i][2] << endl;
-    }
-    cout << endl;
-    for (int i=0; i< outputs.size(); i++)
-    {
-        cout << "salida: " << outputs[i][0] << endl;
-        cout << "tamanio: " << outputs[i][1] << endl;
-        cout << "radix: " << outputs[i][2] << endl;
-    }
-    cout << endl;*/
 
     string dummy_module_name = "PEPE";
     cout << getFSMHead(dummy_module_name, s, inputs, outputs);
