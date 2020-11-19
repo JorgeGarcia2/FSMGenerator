584813ef9c2583abcf0671225a1edba8e15c6d38
David Xchel <davidxchelmh@gmail.com>
2020-11-19 17:23:30 -0600

    Cleaned up

--- Cpp/FSM.hpp
+++ Cpp/FSM.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <sstream>
 #include <regex>
+#include <math.h>
 #include "FM.hpp"
 
 using namespace std;
@@ -113,6 +114,82 @@ inline FSMdictionary getFSMData(string &startState, busInfo &nameInputs, busInfo
         }
     }
     return states;
+}
+
+inline string getFSMHead(string& name, FSMdictionary& States, busInfo& input_list, busInfo& output_list)
+{
+    vector<string> keys_vector;     //Store the dictionary keys inside the keys_vector for later use
+    for(map<string, Line_vector>::iterator it = States.begin(); it != States.end(); ++it)
+    {
+        keys_vector.push_back(it->first);
+    }
+
+    //States[keys_vector[i]][j] gets the j-esim object of type FSMLine, whose key is keys_vector[i]
+    unsigned int number_of_inputs = States[keys_vector[0]][0].get_inputs().size();
+    unsigned int number_of_outputs = States[keys_vector[0]][0].get_outputs().size();
+    unsigned int number_of_states = keys_vector.size();
+    unsigned int required_state_bits = ceil(log2(number_of_states));
+
+    string FSMHead = "";
+
+    FSMHead += "module " + name + "(\n"
+             + "  input reset, clock,";
+
+    unsigned int current_bus_size = 0;          //Create variable for later use
+    unsigned int last_bus_size = 0;             //Create variable for later use
+
+    if (number_of_inputs > 0)
+    {
+        for (unsigned int i = 0; i < input_list.size(); i++)    //For all the inputs of the FSM
+        {
+            current_bus_size = stoi(input_list[i][1]);      //Store the current bus size
+            if (current_bus_size != last_bus_size)          //In case that the bus size has changed...
+            {
+                FSMHead += "\n  input ";                    //...a new input declaration is needed
+                if (current_bus_size > 1)
+                    FSMHead += "[" + to_string(current_bus_size - 1) + ":0] ";
+                FSMHead += input_list[i][0] + ", ";
+            }
+            else
+                FSMHead += input_list[i][0] + ", ";            
+            last_bus_size = current_bus_size;               //Now store the used bus size as "last bus size"
+        }
+    }
+
+    current_bus_size = last_bus_size = 0;
+
+    for (unsigned int i = 0; i < output_list.size(); i++)       //For all the outputs of the FSM
+    {
+        current_bus_size = stoi(output_list[i][1]);         //Store the current bus size
+        if (current_bus_size != last_bus_size)              //In case that the bus size has changed...
+        {
+            FSMHead += "\n  output reg ";                   //...a new output reg declaration is needed
+            if (current_bus_size > 1)
+                FSMHead += "[" + to_string(current_bus_size - 1) + ":0] ";
+            FSMHead += output_list[i][0] + ", ";
+        }
+        else
+            FSMHead += output_list[i][0] + ", ";
+        last_bus_size = current_bus_size;                   //Now store the used bus size as "last bus size"
+    }
+
+    FSMHead.erase(FSMHead.end()-2);             //Remove the last two characters
+    FSMHead.pop_back();                         //This function seems necessary
+
+    string keys_string;                         //Pass the states keys to a string for later use
+    for (const auto &piece : keys_vector) keys_string += piece + ", ";
+    keys_string.erase(keys_string.end()-2);     //Remove the last two characters
+    keys_string.pop_back();                     //This function seems necessary
+
+    FSMHead += ");\n\n";
+    FSMHead += "  typedef enum reg [" + to_string(required_state_bits-1) + ":0] {" + keys_string + "} statetype;\n"
+             + "  statetype state, nextstate;\n\n"
+             + "  //State register\n"
+             + "  always@ (posedge clock or posedge reset)\n"
+             + "    if (reset) state <= " + keys_vector[0] + ";\n"
+             + "    else       state <= nextstate;\n";
+
+    return FSMHead;
 }
 
 inline string getFSMLogic(FSMdictionary dicS, busInfo Ni, busInfo No,string ppal){
--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "FSM.hpp"
 
 using namespace std;
@@ -6,12 +5,12 @@ using namespace std;
 int main(){
 	string fileText, sstate, FSMCode,name;
     busInfo inputs, outputs;
-    FSMdictionary s;
+    FSMdictionary States;
 	
-    s = getFSMData(sstate, inputs, outputs,name);
+    States = getFSMData(sstate, inputs, outputs,name);
 
-    FSMCode = "";
-    FSMCode += getFSMLogic(s,inputs,outputs,sstate);
+    FSMCode = getFSMHead(sstate, States, inputs, outputs);
+    FSMCode += getFSMLogic(States,inputs,outputs,sstate);
     name = name.substr(0,name.size()-4);
     writeFSM(name, FSMCode);
 }
