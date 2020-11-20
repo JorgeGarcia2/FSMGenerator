f85e526ff67e5f386a9fbe8f7e66bcbc4f5a8a5d
David Xchel <davidxchelmh@gmail.com>
2020-11-20 17:05:45 -0600

    FM.py/cpp and main.py/cpp commented

--- Python/FSM.py
+++ Python/FSM.py
@@ -49,9 +49,13 @@ class FSM:
         current_bus_size = 0        #Create variable for later use
         last_bus_size = 0           #Create variable for later use
 
+        # Get the input instantiation
         if (number_of_inputs > 0):
             for i in input_list:
                 current_bus_size = int(i[1])
+
+                # If the size of the current signal is different to the last,
+                # instantiate a new input size, else append it to current size
                 if (current_bus_size != last_bus_size):
                     FSMHead += "\n  input "
                     if (current_bus_size > 1):
@@ -61,8 +65,11 @@ class FSM:
         
         current_bus_size = last_bus_size = 0
 
+        # Get the output instantiation
         for i in output_list:
             current_bus_size = int(i[1])
+                # If the size of the current signal is different to the last,
+                # instantiate a new output size, else append it to current size
             if (current_bus_size != last_bus_size):
                 FSMHead += "\n  output reg "
                 if (current_bus_size > 1):
@@ -77,7 +84,7 @@ class FSM:
 
         FSMHead += (");\n\n"
                 f"  typedef enum reg [{required_state_bits-1}:0] {{{keys_string}}} statetype;\n"
-                f"  statetype state, nextstate;\n\n"    #Should be "  statetype [{required_state_bits-1}:0] state, nextstate;" but EPWave is weird
+                f"  statetype state, nextstate;\n\n"    #Should be "  statetype [{required_state_bits-1}:0] state, nextstate;", but works in EPWave
                     "  //State register\n"
                     "  always@ (posedge clock or posedge reset)\n"
                 f"    if (reset) state <= {keys_list[0]};\n"
@@ -85,57 +92,80 @@ class FSM:
         
         return FSMHead
 
-    #Makes the state logic for the machine
+    #Makes the state and output logic for the machine
     def getFSMLogic(self,dicS,Ni,No,ppal):
+
+        # Initialize state logic block with an always sensitive to the current state and the inputs
         FSMSLogic = "\n  //Next State Logic Block\n  always@(state"
         for i in Ni: FSMSLogic += " or " + i[0]
         FSMSLogic += ")\n  begin\n    case(state)\n"
+        # Initialize state logic block with an always sensitive to the current state
         FSMOLogic = "\n  //Output Logic Block\n  always@(state)\n  begin\n    case(state)\n"
 
+        # Iterate over dictionary keys
         for S in dicS.keys():
+
+            # Append the key as a case without begin in state logic for better readability
             FSMSLogic += "      " + S + ":\n"
             FSMOLogic += "      " + S + ": begin\n"
+
+            # Initialize no if found and iterate over input values
             IF = False
             for i in range(len(dicS[S])):
+
+                # If an if was found before, continue with else if instead of an if
                 if (IF): Temp = "        else if("
                 else: Temp = "        if("
+
+                # Initialize found conditions as false
                 f=False
                 for j in range(len(dicS[S][i][0])):
                     if (dicS[S][i][0][j] != "x" and dicS[S][i][0][j] != "X"):
+
+                        # If there was a condition before, append an "&&"
                         if (f): Temp += " && "
                         Temp += Ni[j][0] + " == " + dicS[S][i][0][j]
                         f=True
-                if (Temp != "        if("):
+                # If conditions were found, append the conditions
+                if (f):
                     IF = True
                     FSMOLogic += Temp + ") begin\n"
                     FSMSLogic += Temp + ")\n"
+                # Append the next state depending on the current state and the conditions
                 FSMSLogic += "          nextstate = " + dicS[S][i][1] + ";\n\n"
+
+                # Append the output values for all outputs depending on the current state and the conditions
                 for j in range(len(No)):
                     if (dicS[S][i][2][j] != "x" and dicS[S][i][2][j] != "X"):
                         FSMOLogic += "          " + No[j][0] + " = " + dicS[S][i][2][j] + ";\n"
+                # If there was an if clause, append the end to the output logic
                 if (IF):
                     FSMOLogic += "        end\n"
                 FSMOLogic += "\n"
+            # If there was an if found, finish next state logic with
+            # else going to the same current state
             if (IF):
                 FSMSLogic += "        else\n          nextstate = " + S + ";\n"
-                """FSMOLogic += "        else begin\n"
-                for j in range(len(No)):
-                    if (dicS[S][0][2][j] != "x" and dicS[S][0][2][j] != "X"):
-                        FSMOLogic += "          " + No[j][0] + " = " + dicS[S][0][2][j] + ";\n"
-                    else:
-                        FSMOLogic += "          " + No[j][0] + " = " + No[j][1] + "'" + No[j][2] + "0;\n"
-                FSMOLogic += "        end\n" """
             FSMSLogic += "\n"
             FSMOLogic += "      end\n"
+        # Append default state cases
         FSMSLogic += "      default:\n        nextstate = " + ppal + ";\n"
         FSMOLogic += "      default: begin\n"
+
+        # Append all values of outputs
         for j in range(len(No)):
             FSMOLogic += "        " + No[j][0] + " = " + No[j][1] + "'" + No[j][2] + "0;\n"
+
+        # Finish the next state and output logic block strings
         FSMOLogic += "      end\n"
         FSMSLogic += "    endcase\n  end\n"
         FSMOLogic += "    endcase\n  end\n\nendmodule"
+
+        # Return sum of both strings
         return FSMSLogic + FSMOLogic
 
+    # Function to write string to the file with the key PyDesign
+    # for better organization and v extension
     def writeFSM(self,nam,cont):
         f = open(nam + "_PyDesign.v","w")
         f.write(cont)
